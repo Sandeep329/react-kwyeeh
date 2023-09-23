fdf0719d94a0dfc8a71a03ff7761b3c50066d743
Sandeep329 <39436378+Sandeep329@users.noreply.github.com>
2023-09-23 17:53:53 +0530

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,29 @@
+export const formFields = {
+  fields: [
+    {
+      name: 'firstName',
+      type: 'text',
+    },
+    {
+      name: 'secondName',
+      type: 'text',
+    },
+    {
+      name: 'college',
+      type: 'text',
+    },
+    {
+      name: 'city',
+      type: 'text',
+    },
+    {
+      name: 'country',
+      type: 'text',
+    },
+  ],
+};
+
+export const formState = formFields.fields.reduce((acc, { name, type }) => {
+  acc = { ...acc, [name]: '' };
+  return acc;
+}, {});
